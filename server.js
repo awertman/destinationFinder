fa94a66a1b0af2c3fab090512acd368903c2c264
Aaron Wertman <aaron.wertman@gmail.com>
2014-06-04 12:07:19 -0700

    added additional npm dependency

--- server.js
+++ server.js
@@ -2,27 +2,22 @@
 
   // app setup =====================
   var express  = require('express');
-  var application      = express();                 // create our application w/ express
-  var mongoose = require("mongoose");               // mongo ORM
+  var application      = express();
+  var mongoose = require("mongoose");
+  var dotenv = require('dotenv');
+  dotenv.load();
 
   // configuration =================
   application.configure(function() {
-    application.use(express.static(__dirname + '/public'));     // set the static files location /public/img will be /img for users
-    application.use(express.logger('dev'));                     // log every request to the console
-    application.use(express.bodyParser());                      // pull information from html in POST
+    application.use(express.static(__dirname + '/public'));
+    application.use(express.logger('dev'));
+    application.use(express.bodyParser());
   });
 
 // db set up ==============================================================================================================================
 
-// info
-  //
-  // mongoose.connect(process.env.MONGOHQ_URL) add environment key for login
-  // var MONGOHQ_URL="mongodb://developer:d3v3l0p3r@oceanic.mongohq.com:10095/development_sandbox.destinations"
-  var MONGOHQ_URL="mongodb://developer:d3v3l0p3r@oceanic.mongohq.com:10095/development_sandbox"
-//
-
   // db initialization
-  mongoose.connect(MONGOHQ_URL);
+  mongoose.connect(process.env.MONGOHQ_URL)
   var db = mongoose.connection;
   db.on('error', console.error.bind(console, '---------------------------------------------- connection error'));
   db.once('open', function callback () {
@@ -46,10 +41,9 @@
 
 // application ==========================================================================================================================+
   application.get('*', function(req, res) {
-    res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+    res.sendfile('./public/index.html');
   });
 
-  // listen (start app with node server.js) ===================
   application.listen(8080);
   console.log("---------- App listening on port 8080 ----------");
 
